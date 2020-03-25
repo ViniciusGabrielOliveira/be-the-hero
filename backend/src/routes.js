63bc5466333c3ebce70580750ecfbcde131f0b37
ViniciusGabrielOliveira <viniciusgabriel.oliveira@gmail.com>
2020-03-25 20:23:00 -0300

    paginas register e login

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -10,13 +10,12 @@ const routes = express.Router();
 routes.post('/sessions', SessionController.create);
 
 routes.get('/ongs', OngController.index);
-routes.post('/ongs', OngController);
+routes.post('/ongs', OngController.create);
 
 routes.get('/profile', ProfileController.index);
 
 routes.get('/incidents', IncidentController.index);
 routes.post('./incidents', IncidentController.create);
-
 routes.delete('./incidents/:id', IncidentController.delete);
 
 module.exports = routes; 
